6d7439d6cc20cc658fd4752ea773e3dd263f00a5
cynthia nweke <nwekecynthia2015@gmail.com>
2022-09-29 14:37:05 +0100

    completed project

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+var pagebody=document.querySelector(".pagebody")
+
+var explore=document.querySelector(".explore-container")
+
+
+
+var mobilelinkscontainer=document.querySelector('.mobilelinks-container')
+
+function addandremove(){
+    explore.classList.toggle('showbutton')
+    mobilelinkscontainer.classList.toggle('showmobilelinks')
+}
+
